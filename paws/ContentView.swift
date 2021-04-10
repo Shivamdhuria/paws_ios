3c0015f50981238bdfce999a65a0894892741146
Shivam Dhuria <sdhuria@ask-fast.com>
2021-04-10 14:21:02 +0530

    Initial Commit

---
+++ paws/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  paws
+//
+//  Created by Shivam Dhuria on 10/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ paws/pawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  pawsApp.swift
+//  paws
+//
+//  Created by Shivam Dhuria on 10/04/21.
+//
+
+import SwiftUI
+
+@main
+struct pawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
